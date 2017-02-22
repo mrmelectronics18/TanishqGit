ed34b8bc7be945c380318f1bc2c0465a3a2805e3
mrmelectronics18 <prototypemrm2017@gmail.com>
2017-02-22 18:53:28 +0530

    Add files via upload

---
+++ PWM.py
@@ -0,0 +1,19 @@
+import time
+import Rpi.GPIO as GPIO
+
+LedPin = 12
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(ledpin,GPIO.OUT)
+
+PwmLed = GPIO.PWM(LedPin,500)
+PwmLed.start(0)
+
+while True:
+	for i in range (0,100,5)
+		PwmLed.ChangeDutyCycle(i)
+		time.sleep(0.1)
+	for i in range (100,0,-5)
+		PwmLed.ChangeDutyCycle(i)
+		time.sleep(0.1)	
+
+GPIO.cleanup()
