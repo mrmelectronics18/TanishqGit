b021a0ef86860351667b6b786e63e3852aa11e4b
mrmelectronics18 <prototypemrm2017@gmail.com>
2017-02-24 20:13:04 +0530

    Motor Code

--- Motor_Code.py
+++ Motor_Code.py
@@ -1,10 +1,10 @@
 import pygame
 import RPi.GPIO as GPIO
 
-RFpin =
-RBpin =
-LFpin =
-LBpin =
+RFpin = 
+RBpin = 
+LFpin = 
+LBpin = 
 
 GPIO.setmode (GPIO.BOARD)
 GPIO.setup(RF,GPIO.OUT)
@@ -44,9 +44,6 @@ while done==False:
 
 		X = j.get_axis(0)
 		Y = j.get_axis(1)
-			
-		print "X = ", X
-		print "Y = ", Y
 
 	#Start Motor Code
 	
@@ -56,29 +53,107 @@ while done==False:
 		RB.ChangeDutyCycle(0)
 		LF.ChangeDutyCycle(0)
 		LB.ChangeDutyCycle(0)
+		print "Buffer","RF = ",RF,"RB = ",RB,"LF = ",LF,"LB = ",LB
 
-	if X<0.1 and X>-0.1 and Y<-0.1: #Forward
+	elif X<0.1 and X>-0.1 and Y<-0.1: #Forward
 		RF.ChangeDutyCycle(-Y*100)
 		RB.ChangeDutyCycle(0)
 		LF.ChangeDutyCycle(-Y*100)
 		LB.ChangeDutyCycle(0)
+		print "Forward","RF = ",RF,"RB = ",RB,"LF = ",LF,"LB = ",LB
 
-	if X<0.1 and X>-0.1 and Y>0.1: #Backward
+	elif X<0.1 and X>-0.1 and Y>0.1: #Backward
 		RF.ChangeDutyCycle(0)
 		RB.ChangeDutyCycle(Y*100)
 		LF.ChangeDutyCycle(0)
 		LB.ChangeDutyCycle(Y*100)
+		print "Backward","RF = ",RF,"RB = ",RB,"LF = ",LF,"LB = ",LB
 
-	if Y<0.1 and Y>-0.1 and X>0.1: #Right
+	elif Y<0.1 and Y>-0.1 and X>0.1: #Right
 		RF.ChangeDutyCycle(0)
 		RB.ChangeDutyCycle(X*100)
 		LF.ChangeDutyCycle(X*100)
 		LB.ChangeDutyCycle(0)
+		print "Right","RF = ",RF,"RB = ",RB,"LF = ",LF,"LB = ",LB
 
-	if Y<0.1 and Y>-0.1 and X<-0.1: #Left
+	elif Y<0.1 and Y>-0.1 and X<-0.1: #Left
 		RF.ChangeDutyCycle(-X*100)
 		RB.ChangeDutyCycle(0)
 		LF.ChangeDutyCycle(0)
-		LB.ChangeDutyCycle(-X*100)								
+		LB.ChangeDutyCycle(-X*100)
+		print "Left","RF = ",RF,"RB = ",RB,"LF = ",LF,"LB = ",LB
+
+		elif Y<(-0.1) and X>(0.1): #First Quadrant
+		Y1 = -Y
+		if Y1/X>=1: #Right of Forward
+			Y2 = Y1 - X
+			RF.ChangeDutyCycle((Y2)*100)
+			RB.ChangeDutyCycle(0)
+			LF.ChangeDutyCycle((X)*100)
+			LB.ChangeDutyCycle(0)
+			print "Right of Forward","RF = ",RF,"RB = ",RB,"LF = ",LF,"LB = ",LB," X = ",X," Y = ",Y,"First Quadrant"
+
+		else: #Up of Right
+			Y2 = Y1 - X
+			RF.ChangeDutyCycle(0)
+			RB.ChangeDutyCycle(-(Y2*100))
+			LF.ChangeDutyCycle((X)*100)
+			LB.ChangeDutyCycle(0)
+			print "Up of Right","RF = ",RF,"RB = ",RB,"LF = ",LF,"LB = ",LB," X = ",X," Y = ",Y,"First Quadrant"
+
+	elif Y<(-0.1) and X<(-0.1): #Second Quadrant
+		X1 = -X
+		Y1 = -Y
+		if Y1/X1>=1: #Left of Forward
+			X2 = X1 - Y1
+			RF.ChangeDutyCycle((Y1)*100)
+			RB.ChangeDutyCycle(0)
+			LF.ChangeDutyCycle(-(X2*100))
+			LB.ChangeDutyCycle(0)
+			print "Left of Forward","RF = ",RF,"RB = ",RB,"LF = ",LF,"LB = ",LB," X = ",X," Y = ",Y,"Second Quadrant"
+
+		else: #Up of Left
+			X2 = Y1 - X1
+			RF.ChangeDutyCycle((Y1)*100)
+			RB.ChangeDutyCycle(0)
+			LF.ChangeDutyCycle(0)
+			LB.ChangeDutyCycle(-(X2*100))
+			print "Up of Left","RF = ",RF,"RB = ",RB,"LF = ",LF,"LB = ",LB," X = ",X," Y = ",Y,"Second Quadrant"
+
+	elif Y>(0.1) and X<(-0.1): #Third Quadrant
+		X1 = -X
+		if Y/X1>=1: #Left of Backward
+			Y2 = Y - X1
+			RF.ChangeDutyCycle(0)
+			RB.ChangeDutyCycle((Y2)*100)
+			LF.ChangeDutyCycle(0)
+			LB.ChangeDutyCycle((X1)*100)
+			print "Left of Backward","RF = ",RF,"RB = ",RB,"LF = ",LF,"LB = ",LB," X = ",X," Y = ",Y,"Third Quadrant"
+
+		else: #Down of Left
+			Y2 = Y - X1
+			RF.ChangeDutyCycle(-(Y2*100))
+			RB.ChangeDutyCycle(0)
+			LF.ChangeDutyCycle(0)
+			LB.ChangeDutyCycle((X1)*100)
+			print "Down of Left","RF = ",RF,"RB = ",RB,"LF = ",LF,"LB = ",LB," X = ",X," Y = ",Y,"Third Quadrant"
+
+	elif Y>(0.1) and X>(0.1): #Fourth Quadrant
+		if Y/X>=1: #Right of Backward
+			Y1 = Y - X
+			RF.ChangeDutyCycle((Y1)*100)
+			RB.ChangeDutyCycle(0)
+			LF.ChangeDutyCycle((X)*100)
+			LB.ChangeDutyCycle(0)
+			print "Right of Backward","RF = ",RF,"RB = ",RB,"LF = ",LF,"LB = ",LB," X = ",X," Y = ",Y,"Fourth Quadrant"
+
+		else: #Down of Right
+			Y1 = Y - X
+			RF.ChangeDutyCycle(0)
+			RB.ChangeDutyCycle(-(X*100))
+			LF.ChangeDutyCycle((Y1)*100)
+			LB.ChangeDutyCycle(0)
+			print "Down of Right","RF = ",RF,"RB = ",RB,"LF = ",LF,"LB = ",LB," X = ",X," Y = ",Y,"Fourth Quadrant"
+		
 pygame.quit()					
 GPIO.cleanup()
