4e77849b1b53343f893f269d0350a9e43250109d
mrmelectronics18 <prototypemrm2017@gmail.com>
2017-02-27 20:00:43 +0530

    Update I2C.py

--- I2C.py
+++ I2C.py
@@ -4,26 +4,28 @@ import RPi.GPIO as GPIO
 GPIO.setmode(GPIO.BOARD)
 
 b = smbus.SMBus(1)
-addr = 0x49
-
+addr = 0x3A
+bus.write_quuick(addr)
+b.write_byte_data(addr,0x0F,0x49)
 b.write_byte_data(addr,0x12,0xE0)
 b.write_byte_data(addr,0x20,0x57)
 
 while True:
-	x = b.read_word_data(addr,0x08)
+	b.write_quick(0x3B)
+	x = b.read_byte_data(addr,0x08)
 	print "Mag X = ",x
 
-	y = b.read_word_data(addr,0x0A)
+	y = b.read_byte_data(addr,0x0A)
 	print "Mag Y = ",y
 
-	z = b.read_word_data(addr,0x0C)
+	z = b.read_byte_data(addr,0x0C)
 	print "Mag Z = ",z
 
-	x1 = b.read_word_data(addr,0x28)
+	x1 = b.read_byte_data(addr,0x28)
 	print "Accl X = ",x1
 
-	y1 = b.read_word_data(addr,0x2B)
+	y1 = b.read_byte_data(addr,0x2B)
 	print "Accl Y = ",y1
 
-	z1 = b.read_word_data(addr,0x2D)
+	z1 = b.read_byte_data(addr,0x2D)
 	print "Accl Z = ",z1
